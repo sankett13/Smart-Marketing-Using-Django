45ede2005453be29b6eb3bea098458e11956a62a
Shubham Patel <sanket13052004@gmail.com>
2025-01-29 22:08:44 +0530

    index template

--- Marketing/models.py
+++ Marketing/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
 class User_info(models.Model):
     name=models.CharField( max_length=50)
     email=models.EmailField( max_length=254)
