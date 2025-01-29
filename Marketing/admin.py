1eaa2c4be8feb69a364fcc0a9782c460b8f9de76
INSANITY6406 <skshaikhsahal064@gmail.com>
2025-01-29 16:09:39 +0530

    Created All Models and Resolved One Issue

--- Marketing/admin.py
+++ Marketing/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User_info)
+admin.site.register(Product)
+admin.site.register(user_history)
+admin.site.register(Category)
