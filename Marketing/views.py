625af9c2b3813cded2f6d11386928e175f60c31a
INSANITY6406 <skshaikhsahal064@gmail.com>
2025-01-31 21:00:07 +0530

    Added AI Command Prompt

--- Marketing/views.py
+++ Marketing/views.py
@@ -5,7 +5,12 @@ from django.contrib.auth import authenticate,login
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import *
+from faker import Faker
 from .serializer import *
+import random
+import json
+from groq import Groq
+# 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
 # Create your views here.
 
 class TestView(APIView):
@@ -32,8 +37,81 @@ def login_view(request):
         if user is not None:
             if user.is_superuser:
                 login(request,user)
-                return render(request,"dashboard.html")
+                return redirect("dashboard")
         else:
             return HttpResponse("Invalid credentials")
 
     return render(request,"login.html")
+
+
+def dashboard(request):
+    # fake=Faker()
+    # users=[]
+    # for i in range(5):
+    #     user=User_info.objects.create(
+    #         name=fake.name(),
+    #         email=fake.email(),
+    #         phone_number=fake.phone_number(),
+            
+    #     )
+    #     users.append(user)
+    # categories=[]
+    # for i in range(5):
+    #     category=Category.objects.create(
+    #         category_name=fake.word()
+    #     )
+    #     categories.append(category)
+        
+    # products=[]   
+    # for i in range(10):
+    #     product=Product.objects.create(
+    #         product_name=fake.word(),
+    #         category=random.choice(categories),
+    #         price=round(random.uniform(10,1000),2),
+    #         available_stock=random.randint(1,10),
+            
+    #     )
+    #     products.append(product)
+    # for i in range(20):
+    #     user_history.objects.create(
+    #         user_id=random.choice(users),
+    #         product_id=random.choice(products),
+    #         quantity=random.randint(1,10)
+    #     )
+    
+    return render(request,"dashboard.html",{"name":"sahal"})
+
+def generate_result(request):
+    if request.method=="POST":
+        try:
+            data=json.loads(request.body)
+            text=data["prompt"]
+            print(text)
+            client = Groq(
+            api_key="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"
+            )
+            # prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{article}\n\nArticle:"
+            chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": text,
+                },
+                {
+                    "role":"system",
+                    "content":"You are an AI that will helps to search and Scrape News about Trends Regarding propular and traditional foods and drinks of the place asked during a specific season asked"
+                }
+            ],
+            model="deepseek-r1-distill-llama-70b",
+                )
+            result=chat_completion.choices[0].message.content.strip()
+            if result:
+                return JsonResponse({"content":result})
+            
+        except Exception as er:
+            print(er)
+            return JsonResponse({"content":"Error"})
+
+            
+    else:
+        return Response({"content":"Invalid Request Method"})
